c771b6a22f9a716b63861852486fcf3d8a7d317d
ali1k <hyperir@gmail.com>
2015-05-10 23:15:36 +0200

    started...

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright 2015, Ali Khalili (hyperir@gmail.com).
+ * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
+ */
+'use strict';
+
+module.exports = {
+    LanguageInput: require('./components/LanguageInput')
+};
